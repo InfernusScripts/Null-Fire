cf1ec755a25bc6ad34673fd7b715befbe84ca697
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2025-05-08 13:36:19 +0300

    Update BondFarm.lua

--- Core/Loaders/Dead-Rails/BondFarm.lua
+++ Core/Loaders/Dead-Rails/BondFarm.lua
@@ -34,51 +34,27 @@ local bondFarm; bondFarm = {
 
 		return bond or workspace.RuntimeItems:FindFirstChild("Bond") or tps.ScanFor(game.FindFirstChild, workspace.RuntimeItems, "Bond")
 	end,
-	FoundBond = function(bond)
-		if not found[bond] then
-			found[bond] = true
-			bondFarm.Found += 1
-		end
-		if not cons[bond] and bond:FindFirstChild("Part") and bond.Part:FindFirstChild("Collect") then
-			local collect = bond.Part.Collect
-			local con; con = game:GetService("RunService").RenderStepped:Connect(function()
-				if collect and collect.Playing then
-					bondFarm.Missed -= 1
-					bondFarm.Collected += 1
-					con:Disconnect()
-				elseif not collect.Parent then
-					bondFarm.Missed += 1
-					task.wait(1)
-					con:Disconnect()
-				end
-			end)
-		end
-	end,
 	CheckY = function()
 		local pos = plr.Character:GetPivot().Position
 
-		if pos.Y < -20 then
-			plr.Character:PivotTo(CFrame.new(pos.X, -5, pos.Z))
-		end
-
+		plr.Character.Humanoid.PlatformStand = true
 		plr.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.new(0, 1)
+		plr.Character.HumanoidRootPart.CFrame = CFrame.lookAt(plr.Character.HumanoidRootPart.Position, plr.Character.HumanoidRootPart - Vector3.new(0, 1))
 	end,
 	BondStep = function(bond)
 		if bond then
-			bondFarm.FoundBond(bond)
 			bondFarm.CheckY()
 
 			game:GetService("ReplicatedStorage"):FindFirstChild("C_ActivateObject", math.huge):FireServer(bond)
 
-			pcall(tps.Teleport, bond:GetPivot() - Vector3.new(0, 7.5), nil, nil, false)
+			pcall(tps.Teleport, bond:GetPivot() - Vector3.new(0, 2.5), nil, nil, false)
+			plr.Character.HumanoidRootPart.CFrame = plr.Character.HumanoidRootPart.CFrame:Lerp(bond:GetPivot(), 0.05)
 		end
 	end,
 
 	Finished = false,
 
-	Collected = 0,
-	Found = 0,
-	Missed = 0
+	Collected = 0
 }
 
 getGlobalTable().DRBF = bondFarm
